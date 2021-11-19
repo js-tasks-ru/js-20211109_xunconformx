ae11a85a3427b3a57c0c1778e7a3e9d5a96f113d
k.dubov <example@example.com>
2021-11-19 13:34:55 +0300

    Add create getter

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,17 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+    function getValue(obj, field) {
+        const fieldArr = Array.isArray(field) ? field : field.split('.');
+        const newObj = obj[fieldArr[0]];
 
+        if (newObj && fieldArr.length > 1) {
+            return getValue(newObj, fieldArr.slice(1));
+        }
+
+        return newObj;
+    }
+    return function (item) {
+        return getValue(item, path);
+    };
 }
